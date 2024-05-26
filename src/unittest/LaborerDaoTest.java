e4dbe8895593ace30915e9d831286adbcfd51ec3
verylong <147506109+aeeoou@users.noreply.github.com>
2024-05-26 21:36:58 +0900

    .
    .

--- src/unittest/LaborerDaoTest.java
+++ src/unittest/LaborerDaoTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import com.my.hr.dao.LaborerDao;
 import com.my.hr.dao.LaborerDaoImpl;
 import com.my.hr.domain.Laborer;
+import com.my.hr.domain.NoneException;
 
 // dao 유닛테스트 하기
 public class LaborerDaoTest {
@@ -26,8 +27,30 @@ public class LaborerDaoTest {
 		laborerDao.insertLaborer("박해일", LocalDate.now());
 		System.out.println(laborerDao.selectLaborers());
 		
-		//update 테스트
+		// update 테스트
 		laborerDao.updateLaborer(new Laborer(1, "임화영", LocalDate.of(2024, 5, 26)));
 		System.out.println(laborerDao.selectLaborers());
+		
+		// delete 테스트
+		laborerDao.deleteLaborer(2);
+		System.out.println(laborerDao.selectLaborers());
+		// delete 에서 예외처리가 되는지 테스트하기
+		
+		// 1) update 의 예외처리 테스트
+		try {
+			// 예외가 발새하면 exception이 발생하도록 설계하였다. 
+			laborerDao.updateLaborer(new Laborer(12, null, null)); // 등록되지 않는 정보를 기재해본다.
+		// exception 타입은 NoneException 타입.
+		} catch(NoneException e) {
+			e.printStackTrace();
+		}
+		// 2) delete 의 예외처리 테스트
+		try {
+			laborerDao.deleteLaborer(12); // delete 는 ID 값만 입력하면 된다.
+		} catch(NoneException e) {
+			e.printStackTrace();
+		}
+		
+		// DAO 유닛테스트 끝.
 	}
 }
